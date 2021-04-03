07dc96aabfb566b0a0323a9b33c3c5fcba3a81be
shrijana <shrijana1991@gmail.com>
2021-04-03 18:28:48 +0545

    update epic contract

--- src/config/constants/epic.ts
+++ src/config/constants/epic.ts
@@ -1,6 +1,6 @@
 import { Nft } from './types'
 
-export const NftFarm = '0xb565a75E95ca5e564D14aA3e3E5aD958b6Dac1A4'
+export const NftFarm = '0x61d55B8E38C8D0eE6C52105C0cc59498De0C478d'
 export const NFT = '0xA81Ab2D03b9E3a62BDBb837d417A5E221F754E14'
 export const AMOUNT_TO_CLAIM = '10'
 
