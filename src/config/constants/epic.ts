74aaa8943246ec544a95e48a5308ec540fdab835
shrijana <shrijana1991@gmail.com>
2021-04-03 13:10:24 +0545

    update epic nft data

--- src/config/constants/epic.ts
+++ src/config/constants/epic.ts
@@ -6,18 +6,53 @@ export const AMOUNT_TO_CLAIM = '10'
 
 const Nfts: Nft[] = [
   {
-    name: 'Epic',
-    metadata: 'tier-NFT-base-dark-chocolate.json',
-    description: 'Dipped in chocolate and ready for a bite. Part of the base tier of Vlad NFTs.',
-    previewImage: 'preview-base-dark-chocolate.jpg',
+    name: 'Silver Pepe',
+    metadata: '',
+    description:
+      "The count has had an epic existence sure, but it has not been without many attempts at his life. This artist's rendition pays homage to the time he was frozen in Carbonite ...oh wait, wrong story. Our bad, pretty slick tho right?",
+    previewImage: 'preview-silver-pepe.jpg',
     originalImage:
-      'https://ipfs.io/ipfs/QmX9UuF41nfhnESX3DnVHhC4XwuYAcLEReGyN4CtE8P7Bg?filename=NFT-base-dark-chocolate.mp4',
+      'https://ipfs.io/ipfs/QmaPP3CqB8iNcB9b9NCZVytNiaxZcJWt7Kk1vnHWwRL7hQ?filename=NFT-epic-silver-pepes.mp4',
     fileType: 'mp4',
     blurImage: '',
     sortOrder: 0,
     nftId: 0,
-    tokenAmount: 10,
-    tokenSupply: 2222,
+    tokenAmount: 350,
+    tokenSupply: 66,
+    nftFarmContract: '0x3627Ca89675b42489aD39619A92dd0Ce24CA90bB',
+    nftContract: '0xa521D5FA64D0aAdB4ee607BAb20142aA173e4392',
+  },
+  {
+    name: 'Bank of Vladhalla',
+    metadata: '',
+    description:
+      "They say when you leave this life you can't take it with you... Well, who's leaving? That's for mortals to worry about.",
+    previewImage: 'preview-bank-of-vlad.png',
+    originalImage:
+      'https://ipfs.io/ipfs/Qma87NMJAu33nQuCRvoAK4MwP9CBwhGsLkQdTptM6uxoyp?filename=NFT-epic-bank-of-vladhalla.mp4',
+    fileType: 'mp4',
+    blurImage: '',
+    sortOrder: 1,
+    nftId: 1,
+    tokenAmount: 350,
+    tokenSupply: 66,
+    nftFarmContract: '0x3627Ca89675b42489aD39619A92dd0Ce24CA90bB',
+    nftContract: '0xa521D5FA64D0aAdB4ee607BAb20142aA173e4392',
+  },
+  {
+    name: 'Gate of Vladhalla',
+    metadata: '',
+    description:
+      'Tales have been written of the select few who have made it this far. The  sheer majesty of these very gates would make the hairs on their arms stand on end. Only the immortal shall pass through - will you?',
+    previewImage: 'preview-gate-of-vlad.jpg',
+    originalImage:
+      'https://ipfs.io/ipfs/QmW1az8XriRqdx2zXY7w71XFxXUEfH5hUsyZTzwGBe7gfh?filename=NFT-epic-gates-of-vladhalla.png',
+    fileType: 'png',
+    blurImage: '',
+    sortOrder: 2,
+    nftId: 2,
+    tokenAmount: 350,
+    tokenSupply: 66,
     nftFarmContract: '0x3627Ca89675b42489aD39619A92dd0Ce24CA90bB',
     nftContract: '0xa521D5FA64D0aAdB4ee607BAb20142aA173e4392',
   },
