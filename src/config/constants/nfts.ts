6023429a0dc7bb34bd2597d6cef7b569aa0a47b4
shrijana <shrijana1991@gmail.com>
2021-04-23 20:23:17 +0545

    update nft farm token

--- src/config/constants/nfts.ts
+++ src/config/constants/nfts.ts
@@ -1,6 +1,6 @@
 import { Nft } from './types'
 
-export const NftFarm = '0x6eF08433EA4885177c83fB9698D8B6F9277BeC5c'
+export const NftFarm = '0xaBa054F1437aEBEa43fB0804b734DdB3F39FB62F'
 export const NFT = '0xA81Ab2D03b9E3a62BDBb837d417A5E221F754E14'
 export const AMOUNT_TO_CLAIM = '10'
 
